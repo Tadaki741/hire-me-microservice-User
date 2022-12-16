6cba373ab6cef7ba3f9933d4fa3fd2cc4a2c58dd
Tadaki741 <94947002+Tadaki741@users.noreply.github.com>
2022-12-16 20:23:33 +0700

    Initial commit

---
+++ src/main/java/com/example/hirememicroserviceUser/model/LoginBody.java
@@ -0,0 +1,30 @@
+package com.example.hirememicroserviceUser.model;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class LoginBody {
+
+    @JsonProperty("idToken")
+    private String idToken;
+
+    @JsonCreator
+    public LoginBody(String idToken) {
+        this.idToken = idToken;
+    }
+
+    public String getIdToken() {
+        return idToken;
+    }
+
+    public void setIdToken(String idToken) {
+        this.idToken = idToken;
+    }
+
+    @Override
+    public String toString() {
+        return "LoginBody{" +
+                "idToken='" + idToken + '\'' +
+                '}';
+    }
+}
