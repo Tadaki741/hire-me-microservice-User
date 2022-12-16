134aabb90f71948919ce008e7dc4418c2455a53c
Tadaki741 <94947002+Tadaki741@users.noreply.github.com>
2022-12-16 22:04:58 +0700

    Add firebase dependency

--- src/main/java/com/example/hirememicroserviceUser/HireMeMicroserviceUserApplication.java
+++ src/main/java/com/example/hirememicroserviceUser/HireMeMicroserviceUserApplication.java
@@ -7,6 +7,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class HireMeMicroserviceUserApplication {
 
 	public static void main(String[] args) {
+
+		//Initialize firebase sdk
+
+
+
 		SpringApplication.run(HireMeMicroserviceUserApplication.class, args);
 	}
 
