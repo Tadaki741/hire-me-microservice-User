010bc5939ddf2b56d5a1ccda58da110b61fc1c59
Tadaki741 <94947002+Tadaki741@users.noreply.github.com>
2022-12-16 22:11:15 +0700

    Add code for controller of user

--- src/main/java/com/example/hirememicroserviceUser/controller/UserController.java
+++ src/main/java/com/example/hirememicroserviceUser/controller/UserController.java
@@ -1,10 +1,33 @@
 package com.example.hirememicroserviceUser.controller;
 
 
+import com.example.hirememicroserviceUser.model.LoginBody;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthException;
+import com.google.firebase.auth.FirebaseToken;
+import org.apache.http.HttpStatus;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 @RestController
 public class UserController {
+
+
+    @PostMapping
+    public String login(@RequestBody LoginBody IdTokenFromUser){
+        try {
+            String idToken = IdTokenFromUser.getIdToken();
+            FirebaseToken decodedToken = FirebaseAuth.getInstance().verifyIdToken(idToken);
+            String uid = decodedToken.getUid();
+            return uid;
+        }
+
+        catch (FirebaseAuthException e){
+            throw new ResponseStatusException(HttpStatus.SC_UNAUTHORIZED,"Invalid token ID !",e);
+        }
+    }
 
 
 
--- src/main/java/com/example/hirememicroserviceUser/model/LoginBody.java
+++ src/main/java/com/example/hirememicroserviceUser/model/LoginBody.java
@@ -1,8 +1,28 @@
 package com.example.hirememicroserviceUser.model;
 
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class LoginBody {
 
+    @JsonProperty("idToken")
+    private String idToken;
 
 
+    @JsonCreator
+    public LoginBody(String idToken) {
+        this.idToken = idToken;
+    }
 
+    @Override
+    public String toString() {
+        return "LoginBody{" +
+                "idToken='" + idToken + '\'' +
+                '}';
+    }
 }
