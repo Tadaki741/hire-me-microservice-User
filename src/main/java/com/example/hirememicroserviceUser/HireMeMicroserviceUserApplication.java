0cc143b43ea15c062af787f33d2215389369c56f
Tadaki741 <94947002+Tadaki741@users.noreply.github.com>
2022-12-16 20:23:33 +0700

    Initial commit

--- src/main/java/com/example/hirememicroserviceUser/HireMeMicroserviceUserApplication.java
+++ src/main/java/com/example/hirememicroserviceUser/HireMeMicroserviceUserApplication.java
@@ -1,33 +1,12 @@
 package com.example.hirememicroserviceUser;
 
-import com.google.auth.oauth2.GoogleCredentials;
-import com.google.firebase.FirebaseApp;
-import com.google.firebase.FirebaseOptions;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.util.Objects;
 
 @SpringBootApplication
 public class HireMeMicroserviceUserApplication {
 
-	public static void main(String[] args) throws IOException {
-
-		//Initialize firebase sdk
-		ClassLoader classLoader = HireMeMicroserviceUserApplication.class.getClassLoader();
-        InputStream serviceAccount = classLoader.getResourceAsStream("serviceAccountKey.json");
-
-		//Firebase
-		FirebaseOptions options = FirebaseOptions.builder()
-						.setCredentials(GoogleCredentials.fromStream(serviceAccount))
-								.build();
-
-		FirebaseApp.initializeApp(options);
-
+	public static void main(String[] args) {
 		SpringApplication.run(HireMeMicroserviceUserApplication.class, args);
 	}
 
--- src/main/java/com/example/hirememicroserviceUser/model/LoginBody.java
+++ src/main/java/com/example/hirememicroserviceUser/model/LoginBody.java
@@ -1,21 +1,23 @@
 package com.example.hirememicroserviceUser.model;
 
-
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
-import lombok.Getter;
-import lombok.Setter;
 
-@Getter
-@Setter
 public class LoginBody {
 
     @JsonProperty("idToken")
     private String idToken;
 
-
     @JsonCreator
     public LoginBody(String idToken) {
+        this.idToken = idToken;
+    }
+
+    public String getIdToken() {
+        return idToken;
+    }
+
+    public void setIdToken(String idToken) {
         this.idToken = idToken;
     }
 
