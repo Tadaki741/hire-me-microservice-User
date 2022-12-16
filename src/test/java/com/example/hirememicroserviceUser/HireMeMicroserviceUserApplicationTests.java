d6ea687a26cdf8d1830b88625794880890dc02d9
Tadaki741 <94947002+Tadaki741@users.noreply.github.com>
2022-12-16 22:02:13 +0700

    Initial commit

---
+++ src/test/java/com/example/hirememicroserviceUser/HireMeMicroserviceUserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hirememicroserviceUser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HireMeMicroserviceUserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
