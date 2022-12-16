29e04d4edbcd55b1025166832e8d2cc6767131b6
Tadaki741 <94947002+Tadaki741@users.noreply.github.com>
2022-12-16 20:28:55 +0700

    Add initializer code in SpringbootApplication file

--- src/main/java/com/example/hirememicroserviceUser/HireMeMicroserviceUserApplication.java
+++ src/main/java/com/example/hirememicroserviceUser/HireMeMicroserviceUserApplication.java
@@ -1,13 +1,33 @@
 package com.example.hirememicroserviceUser;
 
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Objects;
 
 @SpringBootApplication
 public class HireMeMicroserviceUserApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(HireMeMicroserviceUserApplication.class, args);
-	}
+    public static void main(String[] args) throws IOException {
+
+        //Initialize the firebase SDK
+        ClassLoader classLoader = HireMeMicroserviceUserApplication.class.getClassLoader();
+        File file = new File(Objects.requireNonNull(classLoader.getResource("serviceAccountKey.json")).getFile());
+        FileInputStream serviceAccount = new FileInputStream(file.getAbsolutePath());
+
+        FirebaseOptions options = FirebaseOptions.builder()
+                .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+                .build();
+
+        FirebaseApp.initializeApp(options);
+
+        SpringApplication.run(HireMeMicroserviceUserApplication.class, args);
+    }
 
 }
