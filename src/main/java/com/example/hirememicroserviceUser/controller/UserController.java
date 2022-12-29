577687a9000839fbd06dc07f0b4cc3bf4a043fd1
Trung Nguyen <44660966+trungne@users.noreply.github.com>
2022-12-29 17:43:05 +0700

    chore: structure filters for endpoints (#7)

--- src/main/java/com/example/hirememicroserviceUser/controller/UserController.java
+++ src/main/java/com/example/hirememicroserviceUser/controller/UserController.java
@@ -1,6 +1,7 @@
 package com.example.hirememicroserviceUser.controller;
 
 
+import com.example.hirememicroserviceUser.HttpResponse.ResponseError;
 import com.example.hirememicroserviceUser.model.LoginBody;
 import com.example.hirememicroserviceUser.model.User;
 import com.example.hirememicroserviceUser.service.AuthenticationService;
@@ -8,18 +9,20 @@ import com.example.hirememicroserviceUser.service.UserService;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseAuthException;
 import com.google.firebase.auth.FirebaseToken;
-import org.apache.commons.codec.binary.Base64;
-import org.apache.http.HttpStatus;
+import org.springframework.http.HttpStatus;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import com.example.hirememicroserviceUser.HttpResponse.ResponseBody;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
 import java.util.Map;
-import java.util.Optional;
 
-@RestController
+@RestController()
+@RequestMapping("users")
+@CrossOrigin(origins = "*")
 public class UserController {
 
     private static final Logger LOG = LoggerFactory.getLogger(UserController.class);
@@ -27,24 +30,17 @@ public class UserController {
     @Autowired
     UserService userService;
 
-    @GetMapping
+    @GetMapping(path = "/test")
     public String test() {
         return "Test2";
     }
 
-    @PostMapping()
-    public User insertUser(@RequestBody User user) {
-        return userService.save(user);
-    }
-
-
-    @PostMapping(path = "user/auth")
-    public String saveNewUserOrLogin(@RequestHeader (name="Authorization") String loginBody) {
+    @PostMapping(path = "/auth")
+    public String saveNewUserOrLogin(@RequestBody LoginBody loginBody) {
         try {
             //Get the idToken from the login body
-//            String idToken = loginBody.getIdToken();
-            String[] a = loginBody.split(" ");
-            String idToken = a[1];
+            String idToken = loginBody.getIdToken();
+
             //Decode it
             FirebaseToken decodedToken = FirebaseAuth.getInstance().verifyIdToken(idToken);
 
@@ -70,7 +66,7 @@ public class UserController {
 
         } catch (FirebaseAuthException e) {
             //Send back error for invalid IdToken
-            throw new ResponseStatusException(HttpStatus.SC_UNAUTHORIZED, "Invalid Token ID!", e);
+            throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "Invalid Token ID!", e);
         }
     }
 
@@ -83,10 +79,24 @@ public class UserController {
     }
 
     // Get user by email
-    @GetMapping("/get/{email}")
-    public User findByEmail(@PathVariable("email") final String email) {
+    @GetMapping("/email/{email}")
+    public ResponseEntity<ResponseBody> findByEmail(@PathVariable("email") final String email) {
         LOG.info("Fetching employee with email = " + email);
-        return userService.findByEmail(email);
+        User user = userService.findByEmail(email);
+
+        if (user == null){
+            ResponseBody body = new ResponseBody(null, new ResponseError("User doesn't exist", 404));
+            return new ResponseEntity<>(body, HttpStatus.NOT_FOUND);
+        }
+        ResponseBody body = new ResponseBody(user);
+        return new ResponseEntity<>(body, HttpStatus.OK);
+    }
+
+    @PostMapping()
+    public ResponseEntity<ResponseBody> insert(@RequestBody User user){
+        User newUser = userService.save(user);
+        ResponseBody body = new ResponseBody(newUser);
+        return new ResponseEntity<>(body, HttpStatus.OK);
     }
 
     // Delete user by email.
