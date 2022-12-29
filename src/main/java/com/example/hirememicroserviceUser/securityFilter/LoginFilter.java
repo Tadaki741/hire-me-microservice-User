31d924a0af8e0bf2846e1d49d39da77f41ce9f28
Trần Đăng Khoa <61503687+khoatran1602@users.noreply.github.com>
2022-12-29 22:00:14 +0700

    Adding decode + Validate JWT (#10)
    Decoding JWT by Base64.
    Validating JWT by secretKey from application.properties

--- src/main/java/com/example/hirememicroserviceUser/securityFilter/LoginFilter.java
+++ src/main/java/com/example/hirememicroserviceUser/securityFilter/LoginFilter.java
@@ -1,8 +1,11 @@
 package com.example.hirememicroserviceUser.securityFilter;
 
 
+import com.example.hirememicroserviceUser.model.User;
 import com.example.hirememicroserviceUser.service.AuthenticationService;
+import io.jsonwebtoken.Jwts;
 import lombok.NonNull;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
@@ -11,11 +14,14 @@ import javax.servlet.*;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Base64;
+import java.util.Map;
 import java.util.logging.Logger;
 
 @Component
 public class LoginFilter extends OncePerRequestFilter {
-
+    @Value("${JWT_SECRET_KEY}")
+    private String secretKey;
     private static final Logger logger = Logger.getLogger(LoginFilter.class.getName());
 
     @Override
@@ -30,13 +36,22 @@ public class LoginFilter extends OncePerRequestFilter {
     @Override
     protected void doFilterInternal(@NonNull HttpServletRequest request, @NonNull HttpServletResponse response, @NonNull FilterChain filterChain) throws ServletException, IOException {
         String headerToken = AuthenticationService.extractTokenStringFromHeader(request);
-        if (headerToken == null){
+        if (headerToken == null) {
             response.sendError(HttpStatus.UNAUTHORIZED.value(), "Invalid JWT");
             return;
         }
 
         // TODO: decode and validate jwt token here
-        filterChain.doFilter(request, response);
+        try {
+            String validation = AuthenticationService.decodeJWTToken(headerToken);
+            if (validation == null) {
+                response.sendError(HttpStatus.UNAUTHORIZED.value(), "JWT NOT VALIDATED");
+                return;
+            }
+            filterChain.doFilter(request, response);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
--- src/main/java/com/example/hirememicroserviceUser/service/AuthenticationService.java
+++ src/main/java/com/example/hirememicroserviceUser/service/AuthenticationService.java
@@ -3,14 +3,21 @@ package com.example.hirememicroserviceUser.service;
 import com.example.hirememicroserviceUser.model.User;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.impl.crypto.DefaultJwtSignatureValidator;
+import org.springframework.beans.factory.annotation.Value;
 
+import javax.crypto.spec.SecretKeySpec;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.util.Base64;
 import java.util.Date;
 import java.util.Map;
 
-public class AuthenticationService {
+import static io.jsonwebtoken.SignatureAlgorithm.HS256;
 
+public class AuthenticationService {
+    @Value("${JWT_SECRET_KEY}")
+    private static String secretKey;
     static final long JWT_EXPIRATION = 864_000_00; // equal 1 day in milliseconds
     static final String JWT_SECRET = "SecretKey";
 
@@ -58,9 +65,31 @@ public class AuthenticationService {
                 .setIssuedAt(now)
                 .setExpiration(expiryDate)
                 .setClaims(Map.of("email", user.getEmail(), "isRecruiter", user.getIsRecruiter()))
-                .signWith(SignatureAlgorithm.HS512, JWT_SECRET)
+                .signWith(SignatureAlgorithm.HS512, secretKey)
                 .compact();
     }
 
+    public static String decodeJWTToken(String token) throws Exception {
+        //Extract header and payload in JWT
+        String[] jwt = token.split("\\.");
+        Base64.Decoder decoder = Base64.getUrlDecoder();
+        String header = new String(decoder.decode(jwt[0]));
+        String payload = new String(decoder.decode(jwt[1]));
+
+        String tokenWithoutSignature = jwt[0] + "." + jwt[1];
+        String signature = jwt[2];
+
+        //Validation part
+        SignatureAlgorithm sa = HS256;
+        SecretKeySpec secretKeySpec = new SecretKeySpec(secretKey.getBytes(), sa.getJcaName());
+
+        DefaultJwtSignatureValidator validator = new DefaultJwtSignatureValidator(sa, secretKeySpec);
+        //If token is not validated, return null
+        if (!validator.isValid(tokenWithoutSignature, signature)) {
+            return null;
+        }
+        //return header and payload
+        return header + " " + payload;
+    }
 
 }
