01e2f4d2426ac75926de4eaa7ab7cff579734ebd
trungne <nguyenquochoangtrung@gmail.com>
2023-01-04 00:48:35 +0700

    chore: fix verify token

--- src/main/java/com/example/hirememicroserviceUser/model/LoginBody.java
+++ src/main/java/com/example/hirememicroserviceUser/model/LoginBody.java
@@ -2,7 +2,9 @@ package com.example.hirememicroserviceUser.model;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.ToString;
 
+@ToString
 public class LoginBody {
 
     @JsonProperty("idToken")
